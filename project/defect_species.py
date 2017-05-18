4ec30b23696a0c839b7cc2f37a406b86560205ae
Benjamin Morgan <benmorgan2@gmail.com>
2017-05-18 08:42:13 +0100

    Added docstring to Defect_Species.__init__()

--- project/defect_species.py
+++ project/defect_species.py
@@ -1,7 +1,22 @@
 class Defect_Species:
-    """ The Defect_Species class includes the information about each defect species present in the system. 
-        label refers to what the defect is call ie 'Vo' for an oxygen vacancy. The valence is the charge of the defect and the mole_fraction is the mole fraction of the defect present in the system. fixed refers to whether the system is following a Mott-Schottky (immobile dopant ions) or Gouy Chapman ( mobile dopant ions ) approximation."""
-    def __init__( self, label, valence, mole_fraction, fixed = False ):
+    """ 
+    The Defect_Species class includes the information about each defect species present in the system. 
+    label refers to what the defect is call ie 'Vo' for an oxygen vacancy. The valence is the charge of the defect and the mole_fraction is the mole fraction of the defect present in the system. fixed refers to whether the system is following a Mott-Schottky (immobile dopant ions) or Gouy Chapman ( mobile dopant ions ) approximation.
+    """
+
+    def __init__( self, label, valence, mole_fraction, fixed=False ):
+        """
+        Initialise a Defect_Species object.
+
+        Args:
+            label (str): A string that labels this defect species.
+            valence (float): The effective charge for this defect species.
+            mole_fraction (float): TODO: need to check this definition. Is this conc per site, or overall?
+            fixed (bool:optional): If fixed is True, the defect concentrations will remain fixed at the "bulk" values. Default: False.
+ 
+        Returns:
+            None
+        """
         assert( isinstance( label, str ) )
         assert( isinstance( valence, float ) )
         assert( isinstance( mole_fraction, float ) )
